e307e3dbfc187e69288356416ca545616f555252
Donovan <cotterdonovan@gmail.com>
2021-03-30 11:12:25 -0400

    side navigation component ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MessengerComponent } from './components/messenger/messenger.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'messages/:id', component: MessengerComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MessengerComponent } from './components/messenger/messenger.component';
 
 @NgModule({
-  declarations: [AppComponent, SideNavComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatSidenavModule, LayoutModule],
+  declarations: [AppComponent, SideNavComponent, MessengerComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatSidenavModule, LayoutModule, MatButtonModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent]
 })
