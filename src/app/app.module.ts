4fba0f9dd9492f65ba60c37e0b4889f16456847e
Donovan <cotterdonovan@gmail.com>
2021-03-31 10:54:30 -0400

    displaying mock chat messages in the messenger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,22 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MessengerComponent } from './components/messenger/messenger.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { MatCardModule } from '@angular/material/card';
+import { ChatMessageComponent } from './components/chat-message/chat-message.component';
 
 @NgModule({
-  declarations: [AppComponent, SideNavComponent, MessengerComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatSidenavModule, LayoutModule, MatButtonModule, MatIconModule],
+  declarations: [AppComponent, SideNavComponent, MessengerComponent, ChatComponent, ChatMessageComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    LayoutModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
